9854fb6640cb8c431b3d051b1fee97f41d6eec39
stonarini <stonarini@cifpfbmoll.eu>
2022-02-16 23:48:19 +0100

    test(UfosPark): add test for UfosPark class

---
+++ src/test/java/edu/poniperro/business/services/UfosParkTest.java
@@ -0,0 +1,53 @@
+package edu.poniperro.business.services;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.poniperro.business.payment.CreditCard;
+
+public class UfosParkTest {
+	UfosPark ufosPark;
+	CreditCard creditCard;
+
+	@Before
+	public void setup() {
+		this.creditCard = new CreditCard("Name", "12345ABC");
+		this.ufosPark = new UfosPark();
+		String[] ufos = { "unx", "dox" };
+		for (String ufo : ufos) {
+			ufosPark.add(ufo);
+		}
+	}
+
+	@Test
+	public void dispatchTest() {
+		assertNull(ufosPark.getUfoOf(creditCard.number()));
+		ufosPark.dispatch(creditCard);
+		String ufoOf = ufosPark.getUfoOf(creditCard.number());
+		assertTrue("unx" == ufoOf
+				|| "dox" == ufoOf
+				|| "trex" == ufoOf);
+
+	}
+
+	@Test
+	public void dispatchFlotaOcupadaTest() {
+		ufosPark.dispatch(new CreditCard("OtherName", "Number"));
+		ufosPark.dispatch(new CreditCard("NewName", "OtherNumber"));
+		ufosPark.dispatch(creditCard);
+		assertNull(ufosPark.getUfoOf(creditCard.number()));
+	}
+
+	@Test
+	public void emptyCreditCardTest() {
+		creditCard.pay(3000);
+		ufosPark.dispatch(creditCard);
+		assertNull(ufosPark.getUfoOf(creditCard.number()));
+	}
+		
+	}
+}

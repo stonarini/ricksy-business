cb9271a8e5e86e8b5ed382d1133932aa886c8b25
stonarini <stonarini@cifpfbmoll.eu>
2022-02-17 00:11:26 +0100

    feat(UfosPark): add methods of UfosPark class

--- src/main/java/edu/poniperro/business/services/UfosPark.java
+++ src/main/java/edu/poniperro/business/services/UfosPark.java
@@ -2,6 +2,7 @@ package edu.poniperro.business.services;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import edu.poniperro.business.payment.CreditCard;
 
@@ -13,8 +14,38 @@ public class UfosPark implements GuestDispatcher {
 
 	}
 
+	public void add(String ufo) {
+		this.flota.put(ufo, null);
+	}
+
 	@Override
 	public void dispatch(CreditCard creditCard) {
+		if (!flota.containsValue(creditCard.number()) && creditCard.pay(fee)) {
+			Optional<String> ufo = getFreeUfo();
 
+			if (ufo.isPresent()) {
+				flota.put(ufo.get(), creditCard.number());
+			}
+		}
+
+	}
+
+	private Optional<String> getFreeUfo() {
+		return this.flota.entrySet().stream()
+				.filter(u -> u.getValue() == null)
+				.map(u -> u.getKey())
+				.findAny();
+	}
+
+	public String getUfoOf(String cardNumber) {
+		if (flota.containsValue(cardNumber)) {
+			return flota.entrySet().stream()
+					.filter(u -> u.getValue() == cardNumber)
+					.map(u -> u.getKey())
+					.findFirst()
+					.get();
+		} else {
+			return null;
+		}
 	}
 }

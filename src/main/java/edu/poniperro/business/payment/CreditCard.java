92dc3d3fe873429c4d725ab9aa15c6a2a6a9121b
stonarini <stonarini@cifpfbmoll.eu>
2022-02-16 22:51:54 +0100

    feat(CreditCard): add CreditCard methods

--- src/main/java/edu/poniperro/business/payment/CreditCard.java
+++ src/main/java/edu/poniperro/business/payment/CreditCard.java
@@ -3,14 +3,34 @@ package edu.poniperro.business.payment;
 public class CreditCard {
     private final String owner;
     private final String number;
-    private final double credit;
+    private double credit;
     private final String SYMBOL;
 
-    CreditCard(String owner, String number) {
+    public CreditCard(String owner, String number) {
         this.owner = owner;
         this.number = number;
         this.credit = 3000;
         this.SYMBOL = "EZI";
     }
 
+    public boolean pay(double value) {
+        if (credit() - value >= 0) {
+            this.credit -= value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String number() {
+        return this.number;
+    }
+
+    public String cardOwner() {
+        return this.owner;
+    }
+
+    public double credit() {
+        return this.credit;
+    }
 }
